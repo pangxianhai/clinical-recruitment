976e8360cee502c70e5dc78d1b6c551d7e235aee
xavier.pang <xavier.pang@xgimi.com>
2019-01-08 23:07:06 +0800

    feate(招募列表)：招募列表页

--- recruitment-web/recruitment-boot/src/main/java/com/andy/recruitment/web/controller/IndexController.java
+++ recruitment-web/recruitment-boot/src/main/java/com/andy/recruitment/web/controller/IndexController.java
@@ -1,9 +1,5 @@
 package com.andy.recruitment.web.controller;
 
-import com.xgimi.converter.MyParameter;
-import java.util.Date;
-import java.util.Map;
-import org.hibernate.validator.constraints.Length;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -16,24 +12,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class IndexController {
 
-    public static class UserInfo {
-
-        @Length(max = 3)
-        private String userName;
-
-        public String getUserName() {
-            return userName;
-        }
-
-        public void setUserName(String userName) {
-            this.userName = userName;
-        }
-    }
 
     @RequestMapping(value = "/", method = {RequestMethod.GET, RequestMethod.POST})
-    public String test(Map<String, Object> model, @MyParameter UserInfo userInfo) {
-        model.put("time", new Date());
-        model.put("uNname", userInfo.getUserName());
-        return "test";
+    public String homePage() {
+        return "redirect:/recruitment/list";
     }
 }
--- recruitment-web/recruitment-boot/src/main/java/com/andy/recruitment/web/controller/recruitment/webservice/RecruitmentController.java
+++ recruitment-web/recruitment-boot/src/main/java/com/andy/recruitment/web/controller/recruitment/webservice/RecruitmentController.java
@@ -2,8 +2,11 @@ package com.andy.recruitment.web.controller.recruitment.webservice;
 
 import com.andy.recruitment.recruitment.ao.RecruitmentAO;
 import com.andy.recruitment.recruitment.model.RecruitmentInfo;
+import com.andy.recruitment.recruitment.model.RecruitmentQueryParam;
 import com.andy.recruitment.web.controller.recruitment.response.RecruitmentVO;
 import com.andy.recruitment.web.controller.recruitment.util.RecruitmentUtil;
+import com.xgimi.commons.page.PageResult;
+import com.xgimi.commons.page.Paginator;
 import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -38,5 +41,14 @@ public class RecruitmentController {
         RecruitmentVO recruitmentVO = RecruitmentUtil.transformRecruitmentVO(recruitmentInfo);
         model.put("recruitmentInfo", recruitmentVO);
         return "recruitment/detail";
+    }
+
+    @RequestMapping("/list")
+    public String recruitmentList(Map<String, Object> model) {
+        RecruitmentQueryParam queryParam = new RecruitmentQueryParam();
+        PageResult<RecruitmentInfo> pageResult = recruitmentAO.getRecruitmentInfo(queryParam, new Paginator(1, 5));
+        model.put("recruitmentInfoList", pageResult.getData());
+        model.put("paginator", pageResult.getPaginator());
+        return "recruitment/list";
     }
 }
